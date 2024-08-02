55fa6988b1829250a726129ad27455283ec0c5f1
abhishek96k <abhisheknalawade2711@gmail.com>
2024-08-02 17:35:43 +0530

    Implemented express and graphql setup

---
+++ server/src/app/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+
+export async function initServer() {
+  const app = express();
+
+  app.use(bodyParser.json());
+
+  const graphqlServer = new ApolloServer({
+    typeDefs: `
+      type Query {
+        testHello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        testHello: () => `First query from graphql`,
+      },
+    },
+  });
+
+  await graphqlServer.start();
+
+  app.use("/graphql", expressMiddleware(graphqlServer));
+  
+  return app;
+}
---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import { initServer } from "./app";
+
+async function init() {
+  const app = await initServer();
+  app.listen(8000, () => console.log("Server started on port: 8000"));
+}
+
+init();
