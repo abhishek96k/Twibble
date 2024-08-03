877bc316d7fcf69f45b2db2cb6603cd4421ecb2b
abhishek96k <abhisheknalawade2711@gmail.com>
2024-08-03 13:24:08 +0530

    Implemented prisma and supabase

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -2,11 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "../clients/db";
 
 export async function initServer() {
   const app = express();
 
   app.use(bodyParser.json());
+
+  // prismaClient.user.create({
+  //   data: {
+
+  //   }
+  // })
 
   const graphqlServer = new ApolloServer({
     typeDefs: `
@@ -24,6 +31,6 @@ export async function initServer() {
   await graphqlServer.start();
 
   app.use("/graphql", expressMiddleware(graphqlServer));
-  
+
   return app;
 }
